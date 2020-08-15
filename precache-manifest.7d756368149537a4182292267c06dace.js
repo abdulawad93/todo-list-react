f97fdb8b4323ae868277c0fdb3d3bcd1cf066512
Abdul Rahman Awad <abdulrahman.y.awad@gmail.com>
2020-08-15 14:00:11 -0400

    Updates

---
+++ precache-manifest.7d756368149537a4182292267c06dace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b5146c8a79a26416e7d5d9bdd07aaa",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "dd27e6431c34280730b9",
+    "url": "/todo-list-react/static/js/2.504a88db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list-react/static/js/2.504a88db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10333c2d5a1404a4c4f",
+    "url": "/todo-list-react/static/js/main.0ad35b4d.chunk.js"
+  },
+  {
+    "revision": "826642a5112903c57c77",
+    "url": "/todo-list-react/static/js/runtime-main.22aeb5a4.js"
+  }
+]);
